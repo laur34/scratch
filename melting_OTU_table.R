79980a4336d0ef7e09b6a98ae9a371c6898440ef
laur34 <lv70xo@gmail.com>
2021-06-02 15:00:18 +0200

    two new scripts (unrelated, though)

---
+++ melting_OTU_table.R
@@ -0,0 +1,21 @@
+#Create a long-form data table from our OTU table.
+#17.5.2021 LH
+
+setwd("/home/laur/Downloads/")
+setwd("/media/laur/Extreme_SSD/Esser_NGSeco03052019_combined")
+
+data <- read.table(file="kubiaktest_forDT.csv", header=T, stringsAsFactors = F)
+data <- read.table(file="cleaned_OTU_table.tsv", header=T, stringsAsFactors = F)
+#head(data)
+#setDT(data)
+#head(data)
+#DT.ml <- melt(data, id.vars = "OTU_ID", measure.vars = c("NGSeco21062019_B_005", "NGSeco21062019_B_006",  "NGSeco21062019_B_007", "NGSeco21062019_B_008", "NGSeco21062019_B_009", "NGSeco21062019_B_010"))
+
+#write.csv(DT.ml, file="kubiaktest_melt.csv")
+
+library(tidyr)
+DT.g <- gather(data, variable, value, -OTU_ID)
+
+library(dplyr)
+data_long <- DT.g %>% arrange(variable, -(value))
+write.table(data_long, file="cleaned_OTU_tbl_Esser_gathered.tsv", sep="\t", row.names = T, col.names = NA)
