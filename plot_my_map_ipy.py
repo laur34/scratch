1221296a3048dc1807577edb3120c12945e211f2
laur34 <lv70xo@gmail.com>
2022-09-23 12:47:42 +0200

    Updated version of chloropleth map, slider is smaller, and it has a title

--- plot_my_map_ipy.py
+++ plot_my_map_ipy.py
@@ -9,21 +9,21 @@ df = pd.read_csv('ISO_3166-2_country-code_vs_BOLD_and_LH_countriesMaster.csv', s
 
 data = [dict(type='choropleth',
 			locations = df['3let_LH'].astype(str),
-			z=df['NrSpeciesGBIF'].astype(int),
+			z=df['NrSpeciesBOLD'].astype(int),
 			locationmode='ISO-3',
 			colorscale='Greens')]
 			
 #Append next db data to the data
 data.append(data[0].copy())
 
-data[1]['z'] = df['NrSpeciesBOLD'].astype(int)
+data[1]['z'] = df['NrSpeciesBOLDGBIF'].astype(int)
 
 #And third data now, too
-data.append(data[0].copy())
-data[2]['z'] = df['NrSpeciesBOLDGBIF'].astype(int)
+#data.append(data[0].copy())
+#data[2]['z'] = df['NrSpeciesBOLDGBIF'].astype(int)
 
 #Now, let's build our slider
-databases = ['GBIF','BOLD', 'GBIFspeciesInBOLD']
+databases = ['BOLD', 'GBIFspeciesInBOLD']
 steps = []
 
 for i in range(len(data)):
@@ -31,12 +31,21 @@ for i in range(len(data)):
 	step['args'][1][i] = True
 	steps.append(step)
 
-slider = [dict(active=0, pad={"t": 1}, steps=steps, borderwidth=10)]
+slider = [dict(active=0, pad={"t": 10, "l": 1200, "r": 1200}, steps=steps, borderwidth=10)]
 
 #Plot
-layout = dict(geo=dict(scope='world', projection={'type': 'robinson'}), sliders=slider)
+layout = dict(
+    title="Species counts in databases",
+    font=dict(size=20),
+    #margin=dict(l=40, r=40, t=60, b=40),
+    #annotations=[dict(x=1, y=1)],
+    geo=dict(scope='world', projection={'type': 'robinson'}),
+    sliders=slider
+    )
 
 fig = dict(data=data, layout=layout)
 
+
+#fig.show()
 plotly.offline.iplot(fig)
 
